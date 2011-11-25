ca5faf74af74fe9cb681cb4584134ef8480082e2
Jeff Lam <sha0lin@alumni.carnegiemellon.edu>
2011-11-24 21:15:56 -0500

    added drawing of axes in GraphView

---
+++ Calculator/GraphView.h
@@ -0,0 +1,15 @@
+//
+//  GraphView.h
+//  Calculator
+//
+//  Created by Jeffrey Lam on 11/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GraphView : UIView
+
+@property (nonatomic) CGFloat scale;
+
+@end
---
+++ Calculator/GraphView.m
@@ -0,0 +1,59 @@
+//
+//  GraphView.m
+//  Calculator
+//
+//  Created by Jeffrey Lam on 11/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "GraphView.h"
+#import "AxesDrawer.h"
+
+@implementation GraphView
+
+@synthesize scale = _scale;
+
+#define DEFAULT_SCALE 0.90
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (CGFloat)scale
+{
+    if (!_scale) {
+        return DEFAULT_SCALE;
+    } else {
+        return _scale;
+    }
+}
+
+- (void)setScale:(CGFloat)scale
+{
+    if (scale != _scale) {
+        _scale = scale;
+        [self setNeedsDisplay];
+    }
+}
+
+- (void)drawRect:(CGRect)rect
+{
+    //CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGPoint midPoint; // center of our bounds in our coordinate system
+    midPoint.x = self.bounds.origin.x + self.bounds.size.width/2;
+    midPoint.y = self.bounds.origin.y + self.bounds.size.height/2;
+    
+    CGFloat size = self.bounds.size.width / 2;
+    if (self.bounds.size.height < self.bounds.size.width) size = self.bounds.size.height / 2;
+    size *= self.scale; // scale is percentage of full view size
+    
+    [AxesDrawer drawAxesInRect:self.bounds originAtPoint:midPoint scale:self.scale];
+}
+
+@end
